f28c8a624dec5b11566d34a946b7132c0952cf75
Hayat Tamboli <hayat.tamboli@gmail.com>
2024-06-05 04:44:10 +0530

    game v1 completed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import print_frame_buffer
 
 def main():
     # Step 1: Open the 32-bit PNG image
-    input_image_path = "score_you_have_claimed.png"
+    input_image_path = "newGameStarting.png"
     img = Image.open(input_image_path)
 
     # Step 2: Convert the image to grayscale
